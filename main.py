94addcd8ad6596a2af67dc83b2aa302edde0abf1
Zerbaib <81523999+Zerbaib@users.noreply.github.com>
2022-11-02 15:30:31 +0100

    *

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ def saveW():
     with open('wallet.txt', 'w') as file:
         json.dump(wallet, file)
 
-def startW():
-    with open('wallet.txt', 'r') as file:
-        wallet = json.load(file)
-
 def bourse1(ntndo, prixN):
     prixN = ntndo
     ntndo = prixN + random.randint(-250, 250)
@@ -26,20 +22,29 @@ def bourse1(ntndo, prixN):
     else:
         print(f"le prix de Nintendo est a {ntndo} $ | actuellement =")
 
-def achatVente(wallet, ntndo):
+def achatVente(wallet, ntndo, usd):
     os.system("cls")
     choix2 = input("que veut tu faire\n1. acheter\n2. vendre\n3. ton compte\n")
     if choix2 == "1":
-        acht = input(f"que veux tu acheter ?\n1. Nintendo | prix: {ntndo}")
+        acht = input(f"que veux tu acheter ?\n1. Nintendo | prix: {ntndo}\n")
         if acht == 1:
             nombreA = input("combient en veux tu ?")
-            wallet[ntndo] = nombreA
+            prixT = nombreA * ntndo
+            if usd > prixT:
+                wallet[ntndo] = nombreA
+                print(f"vous avez bien acheter {nombreA} d'action Nintendo pour le prix de {prixT}")
+            if usd < prixT:
+                print("vous avez pas assez")
             
-startW()
+with open('wallet.txt', 'r') as file:
+    wallet = json.load(file)
+
+usd = wallet.get("usd")
+
 while True:
     choix1 = input("que veux tu faire\n1. acheter\n2. regarder les prix\n")
     if choix1 == "1":
-        achatVente(ntndo)
+        achatVente(wallet, ntndo, usd)
     if choix1 == "2":
         bourse1(ntndo, prixN)
     

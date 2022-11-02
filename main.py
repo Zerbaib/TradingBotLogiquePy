ec5fb19704b0e27e7976f5554890dd54b8043303
Zerbaib <81523999+Zerbaib@users.noreply.github.com>
2022-11-02 14:39:03 +0100

    it's good

--- main.py
+++ main.py
@@ -2,25 +2,17 @@ import random
 import time
 
 bourse = 10
-prix = bourse
-
-def affichageB(bourse, prix):
-    if bourse == prix:
-        print(f"le prix est a {bourse} $ | actuellement =")
-    if bourse < prix:
-        print(f"le prix est a {bourse} $ | actuellement -")
-    if bourse > prix:
-        print(f"le prix est a {bourse} $ | actuellement +")
-
-def calculB(bourse, prix):
-    prix = bourse
-    bourse = bourse + 10 #random.randint(-15, 50)
-    print(bourse)
-    print(prix)
 
 while True:
-    calculB(bourse, prix)
+    prix = bourse
+    bourse = prix + random.randint(-25, 250)
 
-    affichageB(bourse, prix)    
+    if bourse != prix:
+        if bourse < prix:
+            print(f"le prix est a {bourse} $ | actuellement -")
+        if bourse > prix:
+            print(f"le prix est a {bourse} $ | actuellement +")
+    else:
+        print(f"le prix est a {bourse} $ | actuellement =")
     
     time.sleep(1)

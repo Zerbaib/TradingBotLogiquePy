17ea5a779d0e0e2c90b29883002cd6138c58a8e9
Zerbaib <81523999+Zerbaib@users.noreply.github.com>
2022-11-04 19:15:57 +0100

    txt to json

--- saveLoad.py
+++ saveLoad.py
@@ -1,18 +1,18 @@
 import json
 
-with open('wallet.txt', 'r') as file:
+with open('wallet.json', 'r') as file:
     wallet = json.load(file)
-with open('price.txt','r') as file:
+with open('price.json','r') as file:
     price = json.load(file)
 
 def loadDB(wallet, price):
-    with open('wallet.txt', 'r') as file:
+    with open('wallet.json', 'r') as file:
         wallet = json.load(file)
-    with open('price.txt','r') as file:
+    with open('price.json','r') as file:
         price = json.load(file)
 
 def saveDB(wallet, price):
-    with open('wallet.txt', 'w') as file:
+    with open('wallet.json', 'w') as file:
         json.dump(wallet, file)
-    with open('price.txt', 'w') as file:
+    with open('price.json', 'w') as file:
         json.dump(price, file)
